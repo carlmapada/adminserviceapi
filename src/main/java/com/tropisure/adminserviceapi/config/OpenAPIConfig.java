d8b8f14190bf469b7860b7eeaa40972e35b1718e
carlmapada <carl.mapada@gmail.com>
2025-12-03 20:43:27 -0600

    [cmm]-updated swagger config with security scheme

--- src/main/java/com/tropisure/adminserviceapi/config/OpenAPIConfig.java
+++ src/main/java/com/tropisure/adminserviceapi/config/OpenAPIConfig.java
@@ -1,4 +1,33 @@
 package com.tropisure.adminserviceapi.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                )
+                .info(new Info().title("Admin Service API").version("1.0")
+                        .description("Tropisure RESTful API that handles MGA and Carrier creation"));
+    }
 }
+
