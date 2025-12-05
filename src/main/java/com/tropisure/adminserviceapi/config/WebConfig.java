5b451c312e59afcbdaf0b9260d07caa146736266
carlmapada <carl.mapada@gmail.com>
2025-12-05 00:10:35 -0600

    [cmm]-added CORS configuration

--- src/main/java/com/tropisure/adminserviceapi/config/WebConfig.java
+++ src/main/java/com/tropisure/adminserviceapi/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.tropisure.authserviceapi.config;
+package com.tropisure.adminserviceapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
